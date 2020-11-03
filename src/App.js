fd388ddc7cb9acd11e4ac32b80f1b67aacae799a
Hamza Manzoor <35131572+MirzaHamzaManzoor@users.noreply.github.com>
2020-11-03 00:50:18 -0800

    before installing react simple maps

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
-import React,{ useEffect } from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-import MyButton from './components/MyButton'
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
-import MyGrid from './components/MyGrid';
 
 function App() {
-  // useEffect(()=>{
-  //   fetch("https://covid-19-data.p.rapidapi.com/help/countries?format=json", {
-  //     "method": "GET",
-  //     "headers": {
-  //       "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
-  //       "x-rapidapi-key": "008e546fa2msh8fb244dc4dc123ep10bd52jsn5656e1b6379c"
-  //     }
-  //   })
-  //   .then(response => {
-  //     return response.json()
-  //   }).then(json=>console.log(json))
-  //   .catch(err => {
-  //     console.log(err);
-  //   });
-  // },[])
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch("https://rapidapi.p.rapidapi.com/help/countries", {
+        "method": "GET",
+        "headers": {
+          "x-rapidapi-key": "008e546fa2msh8fb244dc4dc123ep10bd52jsn5656e1b6379c",
+          "x-rapidapi-host": "covid-19-data.p.rapidapi.com"
+        }
+      })
+      let data=await response.json()
+
+      console.log(data);
+    }
+    getData()
+  }, [])
   return (
     <React.Fragment>
-     <CssBaseline />
+      <CssBaseline />
       <Container fixed >
-      <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
-     </Container>
-     <MyGrid/>
-     </React.Fragment>
-    )
+        <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
+      </Container>
+    </React.Fragment>
+  )
 }
 
 export default App;
